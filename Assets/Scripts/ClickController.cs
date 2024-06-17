a1d76aab3e39d91f109b0badb251d753badd421d
YooSeungA52 <124012310+YooSeungA52@users.noreply.github.com>
2024-06-17 10:04:41 +0900

    [Update] Btn UI Toggle로 변경

--- Assets/Scripts/ClickController.cs
+++ Assets/Scripts/ClickController.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class ClickController : MonoBehaviour
 {
     public TextMeshProUGUI countText;
-    private int count = 0;
-    private int clickReward = 1;
+
+    [SerializeField] private int count = 0;
+    [SerializeField] private int coin = 0;
+    [SerializeField] private int clickReward = 1; // 클릭 당 보상
+    [SerializeField] private float AutoClickTime = 3.0f; // 자동 클릭 주기
 
     Animator anim;
 
@@ -23,13 +26,30 @@ public class ClickController : MonoBehaviour
 
     void UpdateCountText()
     {
-        countText.text = "Count: " + count.ToString();
+        countText.text = "coin: " + coin.ToString();
     }
 
     public void OnClick()
     {
         anim.SetTrigger("isClick");
-        count += clickReward;
+        count++;
+        coin += clickReward;
         UpdateCountText();
+    }
+
+    public void StartAutoClick() // 자동 클릭
+    {
+        StartCoroutine(AutoClickCoroutine());
+    }
+
+    IEnumerator AutoClickCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(AutoClickTime);
+            count++;
+            coin += clickReward;
+            UpdateCountText();
+        }
     }
 }
